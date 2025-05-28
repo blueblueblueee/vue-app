f5b6c51a22d694c2a51cf9a60dd395c2479d612c
cedric <2361751078@qq.com>
2025-05-28 13:28:56 +0800

    2025-5-28-13:30

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
@@ -10,7 +10,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sansvue-app-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
