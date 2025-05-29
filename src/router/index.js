ca8f6856c285dce7bcecd67d64d9379201a0abab
cedric <2361751078@qq.com>
2025-05-29 11:23:40 +0800

    2025-5-29-11:24

--- src/router/index.js
+++ src/router/index.js
@@ -20,26 +20,31 @@ const routes = [
   {
     path: '/home',
     name: 'home',
+    meta:{title:'首页'},
     component: () => import('../views/home/index.vue'),
     children:[
       {
         path: '/index',
         name: 'index',
+        meta:{title:'首页'},
         component: () => import('../views/home/index/index.vue')
       },
       {
         path: '/user',
         name: 'user',
+        meta:{title:'用户管理'},
         component: () => import('../views/home/user/index.vue')
       },
       {
         path: '/order',
         name: 'order',
+        meta:{title:'订单管理'},
         component: () => import('../views/home/order/index.vue'),
         children:[
           {
             path: '/order/orderInfo',
             name: 'orderInfo',
+            meta:{title:'订单信息'},
             component: () => import('../views/home/order/orderInfo.vue')
           }   
         ]
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,9 @@
         <el-container>
             <NavMenu />
             <el-container>
-                <el-header>Header</el-header>
+                <el-header>
+                  <Breadcrumb />
+                </el-header>
                 <el-main><router-view /></el-main>
             </el-container>
         </el-container>
@@ -11,12 +13,11 @@
 </template>
 
 <script>
+import Breadcrumb from './breadcrumb.vue';
 import NavMenu from './navMenu.vue';
 
 export default {
-    components:{
-        NavMenu
-    },
+  
     data(){
         return {
 
@@ -24,42 +25,14 @@ export default {
     },
     methods:{
 
+    },
+    components:{
+        NavMenu,
+        Breadcrumb
     }
 }
 </script>
 
 <style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+
 </style>
